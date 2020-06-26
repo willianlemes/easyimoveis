4294f63f228b427eba7164352814cf7316c52184
willianlemes <willianlemes94@gmail.com>
2020-06-26 17:49:36 -0300

    Corrigi o nome da tabela de peoples para people

--- src/database/migrations/1593136447818-CreateTablePeople.ts
+++ src/database/migrations/1593136447818-CreateTablePeople.ts
@@ -1,41 +1,40 @@
-import {MigrationInterface, QueryRunner,Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateTablePeople1593136447818 
-    implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-              name: 'peoples',
-              columns: [
-                {
-                  name: 'id',
-                  type: 'uuid',
-                  generationStrategy: 'uuid',
-                  isPrimary: true
-                },
-                {
-                  name: 'name',
-                  type: 'varchar'
-                },
-                {
-                    name:'nickname',
-                    type:'varchar'
-                },
-                {
-                    name:'profile',
-                    type:'varchar'
-                },
-                {
-                    name:'type',
-                    type:'varchar'
-                }
-              ]
-            })
-          );
-    }
+export default class CreateTablePeople1593136447818
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'people',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'nickname',
+            type: 'varchar',
+          },
+          {
+            name: 'profile',
+            type: 'varchar',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('peoples',true);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('people', true);
+  }
 }
