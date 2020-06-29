fccf1505901bd3ee1780286bbbb7f647cc7ec077
Vinicius Passoni <viniciuspassoni97@gmail.com>
2020-06-29 12:27:34 -0300

    Criação da rota inicial person.routes.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import usersRouter from './users.routes';
+import personRoute from './properties.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/persons',personRoute);
 
 export default routes;
---
+++ src/routes/person.routes.ts
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+
+const personRoute = Router();
+
+personRoute.get('/',(request,response)=>{
+    response.json({message:'PersonRoute'});
+});
+
+export default personRoute;
