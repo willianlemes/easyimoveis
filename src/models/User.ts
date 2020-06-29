6d4e1e729b38a44623c0fc24f6c65acf6b38e3c4
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-06-28 22:43:01 -0300

    Altera tipo do id da model Users para string

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
 
   @Column({ name: 'first_name' })
   firstName: string;
