dd8d3908b9bcaf389c14cfe080091a2198bcbc75
Vinicius Passoni <viniciuspassoni97@gmail.com>
2020-06-29 20:55:13 -0300

    Adicionando novas propriedades no modelo Person

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -16,5 +16,53 @@ class Person {
 
   @Column({ name: 'type' })
   type: string;
+
+  @Column({ name: 'genre' })
+  genre: string;
+  
+  @Column({ name: 'datebirth' })
+  datebirth: Date;
+
+  @Column({ name: 'rg_ie' })
+  rg_ie: string;
+
+  @Column({ name: 'cpf_cnpj' })
+  cpf_cnpj: string;
+
+  @Column({ name: 'occupation' })
+  occupation: string;
+
+  @Column({ name: 'email' })
+  email: string;
+
+  @Column({ name: 'phone' })
+  phone: string;
+
+  @Column({ name: 'cell_phone' })
+  cell_phone: string;
+
+  @Column({ name: 'address' })
+  address: string;
+
+  @Column({ name: 'address_number' })
+  address_number: string;
+
+  @Column({ name: 'neighborhood' })
+  neighborhood: string;
+
+  @Column({ name: 'cep' })
+  cep: string;
+
+  @Column({ name: 'state' })
+  state: string;
+
+  @Column({ name: 'city' })
+  city: string;
+
+  @Column({ name: 'photo' })
+  photo: string;
+
+  @Column({ name: 'status' })
+  status: string;
 }
 export default Person;
