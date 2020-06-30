4590521cab5c320056d5a56ec5756a46a11119b3
Vinicius Passoni <viniciuspassoni97@gmail.com>
2020-06-29 21:06:50 -0300

    Adicionando PersonsRepository

---
+++ src/repositories/PersonsRepository.ts
@@ -0,0 +1,15 @@
+import Person from '../models/Person';
+
+class PersonsRepository {
+    private persons: Person[];
+  
+    constructor() {
+      this.persons = [];
+    }
+  
+    findAll(): Person[] {
+      return this.persons;
+    }
+  }
+  
+  export default PersonsRepository;
