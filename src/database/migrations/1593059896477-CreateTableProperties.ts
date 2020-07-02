eb8ca3e94632fd7dfc3202b4cfaf415acd397108
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-07-02 14:07:45 -0300

    Adiciona nulo para propriedades que permitem na model

--- src/database/migrations/1593059896477-CreateTableProperties.ts
+++ src/database/migrations/1593059896477-CreateTableProperties.ts
@@ -17,38 +17,45 @@ export default class CreateTableProperties1593059896477
             name: 'id',
             type: 'uuid',
             generationStrategy: 'uuid',
-            isPrimary: true
+            isPrimary: true,
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'finality',
             type: 'varchar',
-            length: '100'
+            length: '100',
+            isNullable: true
           },
           {
             name: 'kind',
             type: 'varchar',
-            length: '100'
+            length: '100',
+            isNullable: true
           },
           {
             name: 'price',
             type: 'numeric',
             precision: 15,
-            scale: 2
+            scale: 2,
+            isNullable: true
           },
           {
             name: 'address',
             type: 'varchar',
-            length: '255'
+            length: '255',
+            isNullable: true
           },
           {
             name: 'address_number',
             type: 'varchar',
-            length: '10'
+            length: '10',
+            isNullable: true
           },
           {
             name: 'neighborhood',
             type: 'varchar',
-            length: '255'
+            length: '255',
+            isNullable: true
           },
           {
             name: 'complement',
@@ -59,66 +66,79 @@ export default class CreateTableProperties1593059896477
           {
             name: 'cep',
             type: 'varchar',
-            length: '9'
+            length: '9',
+            isNullable: true
           },
           {
             name: 'city',
             type: 'varchar',
-            length: '255'
+            length: '255',
+            isNullable: true
           },
           {
             name: 'state',
             type: 'varchar',
-            length: '2'
+            length: '2',
+            isNullable: true
           },
           {
             name: 'measure_type',
             type: 'varchar',
-            length: '30'
+            length: '30',
+            isNullable: true
           },
           {
             name: 'zone_front',
             type: 'numeric',
             precision: 10,
-            scale: 3
+            scale: 3,
+            isNullable: true
           },
           {
             name: 'zone_bottom',
             type: 'numeric',
             precision: 10,
-            scale: 3
+            scale: 3,
+            isNullable: true
           },
           {
             name: 'zone_left',
             type: 'numeric',
             precision: 10,
-            scale: 3
+            scale: 3,
+            isNullable: true
           },
           {
             name: 'zone_right',
             type: 'numeric',
             precision: 10,
-            scale: 3
+            scale: 3,
+            isNullable: true
           },
           {
             name: 'number_dorms',
-            type: 'int'
+            type: 'int',
+            isNullable: true
           },
           {
             name: 'number_suites',
-            type: 'int'
+            type: 'int',
+            isNullable: true
           },
           {
             name: 'number_bathrooms',
-            type: 'int'
+            type: 'int',
+            isNullable: true
           },
           {
             name: 'number_room',
-            type: 'int'
+            type: 'int',
+            isNullable: true
           },
           {
             name: 'cars_capacity',
-            type: 'int'
+            type: 'int',
+            isNullable: true
           },
           {
             name: 'is_furnished',
