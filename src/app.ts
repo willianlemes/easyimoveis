16b99c7b8c564817be12b67e2030a04fa006f5fe
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-07-02 11:03:57 -0300

    Adiciona cors no backend
    Adiciona lib do cors e o adiciona como middleware da aplicação.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -12,6 +13,7 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
