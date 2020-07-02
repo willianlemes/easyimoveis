12c95f0cfbd242ccab39df7f0d30d0bdc25b4566
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-07-02 16:09:13 -0300

    Adiciona nullable e defaults para Person/People

--- src/database/migrations/1593136447818-CreateTablePeople.ts
+++ src/database/migrations/1593136447818-CreateTablePeople.ts
@@ -11,7 +11,8 @@ export default class CreateTablePeople1593136447818
             name: 'id',
             type: 'uuid',
             generationStrategy: 'uuid',
-            isPrimary: true
+            isPrimary: true,
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'name',
@@ -36,81 +37,97 @@ export default class CreateTablePeople1593136447818
           {
             name: 'genre',
             type: 'varchar',
-            length: '50'
+            length: '50',
+            isNullable: true
           },
           {
             name: 'datebirth',
-            type: 'date'
+            type: 'date',
+            isNullable: true
           },
           {
             name: 'rg_ie',
             type: 'varchar',
-            length: '20'
+            length: '20',
+            isNullable: true
           },
           {
             name: 'cpf_cnpj',
             type: 'varchar',
-            length: '20'
+            length: '20',
+            isNullable: true
           },
           {
             name: 'occupation',
             type: 'varchar',
-            length: '100'
+            length: '100',
+            isNullable: true
           },
           {
             name: 'email',
             type: 'varchar',
-            length: '100'
+            length: '100',
+            isNullable: true
           },
           {
             name: 'phone',
             type: 'varchar',
-            length: '20'
+            length: '20',
+            isNullable: true
           },
           {
             name: 'cell_phone',
             type: 'varchar',
-            length: '20'
+            length: '20',
+            isNullable: true
           },
           {
             name: 'address',
             type: 'varchar',
-            length: '255'
+            length: '255',
+            isNullable: true
           },
           {
             name: 'address_number',
             type: 'varchar',
-            length: '10'
+            length: '10',
+            isNullable: true
           },
           {
             name: 'neighborhood',
             type: 'varchar',
-            length: '255'
+            length: '255',
+            isNullable: true
           },
           {
             name: 'cep',
             type: 'varchar',
-            length: '9'
+            length: '9',
+            isNullable: true
           },
           {
             name: 'state',
             type: 'varchar',
-            length: '2'
+            length: '2',
+            isNullable: true
           },
           {
             name: 'city',
             type: 'varchar',
-            length: '255'
+            length: '255',
+            isNullable: true
           },
           {
             name: 'photo',
             type: 'varchar',
-            length: '255'
+            length: '255',
+            isNullable: true
           },
           {
             name: 'status',
             type: 'varchar',
-            length: '1'
+            length: '1',
+            isNullable: true
           },
           {
             name: 'created_at',
--- src/models/Person.ts
+++ src/models/Person.ts
@@ -31,52 +31,52 @@ class Person {
   @Column({ name: 'type' })
   type: string;
 
-  @Column({ name: 'genre' })
+  @Column({ name: 'genre', nullable: true })
   genre: string;
 
-  @Column({ name: 'datebirth' })
+  @Column({ name: 'datebirth', nullable: true })
   datebirth: Date;
 
-  @Column({ name: 'rg_ie' })
+  @Column({ name: 'rg_ie', nullable: true })
   rg_ie: string;
 
-  @Column({ name: 'cpf_cnpj' })
+  @Column({ name: 'cpf_cnpj', nullable: true })
   cpf_cnpj: string;
 
-  @Column({ name: 'occupation' })
+  @Column({ name: 'occupation', nullable: true })
   occupation: string;
 
-  @Column({ name: 'email' })
+  @Column({ name: 'email', nullable: true })
   email: string;
 
-  @Column({ name: 'phone' })
+  @Column({ name: 'phone', nullable: true })
   phone: string;
 
-  @Column({ name: 'cell_phone' })
+  @Column({ name: 'cell_phone', nullable: true })
   cell_phone: string;
 
-  @Column({ name: 'address' })
+  @Column({ name: 'address', nullable: true })
   address: string;
 
-  @Column({ name: 'address_number' })
+  @Column({ name: 'address_number', nullable: true })
   address_number: string;
 
-  @Column({ name: 'neighborhood' })
+  @Column({ name: 'neighborhood', nullable: true })
   neighborhood: string;
 
-  @Column({ name: 'cep' })
+  @Column({ name: 'cep', nullable: true })
   cep: string;
 
-  @Column({ name: 'state' })
+  @Column({ name: 'state', nullable: true })
   state: string;
 
-  @Column({ name: 'city' })
+  @Column({ name: 'city', nullable: true })
   city: string;
 
-  @Column({ name: 'photo' })
+  @Column({ name: 'photo', nullable: true })
   photo: string;
 
-  @Column({ name: 'status' })
+  @Column({ name: 'status', nullable: true })
   status: string;
 
   @CreateDateColumn({ name: 'created_at', default: 'now()' })
