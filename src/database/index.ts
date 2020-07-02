012f27b8578eb51a0f8860350cbcce3809da03d9
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-07-01 21:03:38 -0300

    Refatora criação da connection com o banco de dados
    1. Remove uso do parâmetro name de database/index.ts
    2. Atribui name como default direto em ormconfig.json

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,7 @@
 import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 
-export default async (name = 'default'): Promise<Connection> => {
+export default async function (): Promise<Connection> {
   const defaultOptions = await getConnectionOptions();
 
-  return createConnection(
-    Object.assign(defaultOptions, {
-      name,
-      database: 'easyimoveis'
-    })
-  );
-};
+  return createConnection(defaultOptions);
+}
