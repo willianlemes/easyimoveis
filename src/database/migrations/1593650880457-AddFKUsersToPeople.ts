10f0095267f66d9e3b83aff8c303662aa26abe3d
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-07-01 21:49:53 -0300

    Remove migration fora do padrão e a recria

---
+++ src/database/migrations/1593650880457-AddFKUsersToPeople.ts
@@ -0,0 +1,36 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey
+} from 'typeorm';
+
+export default class AddFKUsersToPeople1593650880457
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'people',
+      new TableColumn({
+        name: 'users_id',
+        type: 'uuid'
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'people',
+      new TableForeignKey({
+        columnNames: ['users_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        name: 'fk_people_users'
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('people', 'fk_people_users');
+    await queryRunner.dropColumn('people', 'users_id');
+  }
+}
