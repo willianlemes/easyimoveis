cc60e2b4ce81ed1ac679ff096fab7e6816d34860
Vinicius Passoni <viniciuspassoni97@gmail.com>
2020-07-01 21:56:21 -0300

    Terminando models/Person e migration CreateTablePeople

--- src/database/migrations/1593136447818-CreateTablePeople.ts
+++ src/database/migrations/1593136447818-CreateTablePeople.ts
@@ -15,19 +15,112 @@ export default class CreateTablePeople1593136447818
           },
           {
             name: 'name',
-            type: 'varchar'
+            type: 'varchar',
+            length: '255'
           },
           {
             name: 'nickname',
-            type: 'varchar'
+            type: 'varchar',
+            length: '255'
           },
           {
             name: 'profile',
-            type: 'varchar'
+            type: 'varchar',
+            length: '10'
           },
           {
             name: 'type',
-            type: 'varchar'
+            type: 'varchar',
+            length: '1'
+          },
+          {
+            name: 'genre',
+            type: 'varchar',
+            length: '50'
+          },
+          {
+            name: 'datebirth',
+            type: 'date'
+          },
+          {
+            name: 'rg_ie',
+            type: 'varchar',
+            length: '20'
+          },
+          {
+            name: 'cpf_cnpj',
+            type: 'varchar',
+            length: '20'
+          },
+          {
+            name: 'occupation',
+            type: 'varchar',
+            length: '100'
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '100'
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            length: '20'
+          },
+          {
+            name: 'cell_phone',
+            type: 'varchar',
+            length: '20'
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+            length: '255'
+          },
+          {
+            name: 'address_number',
+            type: 'varchar',
+            length: '10'
+          },
+          {
+            name: 'neighborhood',
+            type: 'varchar',
+            length: '255'
+          },
+          {
+            name: 'cep',
+            type: 'varchar',
+            length: '9'
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+            length: '2'
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+            length: '255'
+          },
+          {
+            name: 'photo',
+            type: 'varchar',
+            length: '255'
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+            length: '1'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()'
           }
         ]
       })
