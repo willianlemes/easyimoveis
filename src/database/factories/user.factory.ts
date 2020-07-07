c441fa2d80aee659be0d04893f304e043ebf489a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-07-06 23:10:27 -0300

    Faz uso da criação unique do e-mail e dos Enums

--- src/database/factories/user.factory.ts
+++ src/database/factories/user.factory.ts
@@ -1,24 +1,32 @@
 import { define } from 'typeorm-seeding';
+import '../../extensions/faker';
 import Faker from 'faker';
+
 import User from '../../models/User';
+
+import { Genre, Status } from '../../enums';
 
 define(User, (faker: typeof Faker) => {
   // eslint-disable-next-line no-param-reassign
   faker.locale = 'pt_BR';
+
   const gender = faker.random.number(1);
+
   const user = new User();
   user.firstName = faker.name.firstName(gender);
   user.lastName = faker.name.lastName(gender);
-  user.email = faker.internet
-    .email(user.firstName, user.lastName)
-    .toLowerCase();
+  user.email = faker.custom.uniqueEmail(user.firstName, user.lastName);
   user.password = faker.random.word();
-  user.genre = faker.random.arrayElement<string>([
-    'Masculino',
-    'Feminino',
-    'Outro'
+  user.genre = faker.random.arrayElement<Genre>([
+    Genre.MALE,
+    Genre.FEMALE,
+    Genre.OTHER
   ]);
   user.dateBirth = faker.date.past(80, '2000');
-  user.status = faker.random.arrayElement<string>(['A', 'I']);
+  user.status = faker.random.arrayElement<Status>([
+    Status.ACTIVE,
+    Status.INACTIVE
+  ]);
+
   return user;
 });
