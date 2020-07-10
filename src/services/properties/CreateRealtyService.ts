1e29c7b35bc5b4d3ec68b4f4c4cb0a375a4f4558
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-07-10 00:44:44 -0300

    Extrai Request para raiz de services em Properties

--- src/services/properties/CreateRealtyService.ts
+++ src/services/properties/CreateRealtyService.ts
@@ -2,34 +2,7 @@ import { getRepository } from 'typeorm';
 
 import Realty from '../../models/Realty';
 
-import { Finality, Status } from '../../enums';
-
-interface Request {
-  userId: string;
-  ownerId: string;
-  finality?: Finality;
-  kind?: string;
-  price?: number;
-  address?: string;
-  addressNumber?: string;
-  neighborhood?: string;
-  complement?: string;
-  cep?: string;
-  city?: string;
-  state?: string;
-  measureType?: string;
-  zoneFront?: number;
-  zoneBottom?: number;
-  zoneLeft?: number;
-  zoneRight?: number;
-  numberDorms?: number;
-  numberSuites?: number;
-  numberBathrooms?: number;
-  numberRoom?: number;
-  carsCapacity?: number;
-  isFurnished: boolean;
-  status: Status;
-}
+import Request from './interfaces/Request';
 
 class CreateRealtyService {
   async execute(request: Request): Promise<Realty> {
---
+++ src/services/properties/interfaces/Request.ts
@@ -0,0 +1,28 @@
+import { Finality, Status } from '../../../enums';
+
+export default interface Request {
+  userId: string;
+  ownerId: string;
+  finality?: Finality;
+  kind?: string;
+  price?: number;
+  address?: string;
+  addressNumber?: string;
+  neighborhood?: string;
+  complement?: string;
+  cep?: string;
+  city?: string;
+  state?: string;
+  measureType?: string;
+  zoneFront?: number;
+  zoneBottom?: number;
+  zoneLeft?: number;
+  zoneRight?: number;
+  numberDorms?: number;
+  numberSuites?: number;
+  numberBathrooms?: number;
+  numberRoom?: number;
+  carsCapacity?: number;
+  isFurnished: boolean;
+  status: Status;
+}
