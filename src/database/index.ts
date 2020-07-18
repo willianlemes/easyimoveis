e9df7d49560a7f9c3a8649e7d2acd03417026f99
willianlemes <willianlemes94@gmail.com>
2020-07-18 18:28:19 -0300

    Criação das variáveis de ambiente do typeorm

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,5 @@ import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 
 export default async function (): Promise<Connection> {
   const defaultOptions = await getConnectionOptions();
-
   return createConnection(defaultOptions);
 }
